736ef29f5105ebb99b61c4edbdc86b04758adff0
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-08-13 09:15:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,27 +10,28 @@ export default class Scene {
     this.canvas.height = height || window.innerHeight;
     this.ctx = this.canvas.getContext("2d");
     this.cam = new Transform(new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+    this.fov = 75;
     document.documentElement.appendChild(this.canvas);
     this.draw();
   }
   project(x) {
-    return new Vector2(this.canvas.width/2+x.x/x.z, this.canvas.height/2-x.y/x.z);
+    return new Vector2(this.canvas.width/2+((x.x+this.cam.x)*this.fov)/((x.z+this.cam.z)+this.fov), this.canvas.height/2-((x.y+this.cam.y)*this.fov)/((x.z+this.cam.z)+this.fov));
   }
   polygon(x, y, z) {
     this.objects.push(new Polygon(this.project(x), this.project(y), this.project(z)));
   }
   plane(pos, rot, scale) {
-  var verts = [
-    new Vector3(pos.x-scale.x/2, pos.y, pos.z-scale.z/2),
-    new Vector3(pos.x+scale.x/2, pos.y, pos.z-scale.z/2),
-    new Vector3(pos.x-scale.x/2, pos.y, pos.z+scale.z/2),
-    new Vector3(pos.x+scale.x/2, pos.y, pos.z+scale.z/2)
-  ];
-  var center = verts.reduce((a,b)=>a.add(b), new Vector3(0,0,0)).div(new Vector3(verts.length, verts.length, verts.length));
-  verts = verts.map(v => v.sub(center).rotate(rot).add(center));
-  this.polygon(verts[0], verts[1], verts[2]);
-  this.polygon(verts[2], verts[1], verts[3]);
-}
+    var verts = [
+      new Vector3(pos.x-scale.x/2, pos.y, pos.z-scale.z/2),
+      new Vector3(pos.x+scale.x/2, pos.y, pos.z-scale.z/2),
+      new Vector3(pos.x-scale.x/2, pos.y, pos.z+scale.z/2),
+      new Vector3(pos.x+scale.x/2, pos.y, pos.z+scale.z/2)
+    ];
+    var center = verts.reduce((a,b)=>a.add(b), new Vector3(0,0,0)).div(new Vector3(verts.length, verts.length, verts.length));
+    verts = verts.map(v => v.sub(center).rotate(rot).add(center));
+    this.polygon(verts[0], verts[1], verts[2]);
+    this.polygon(verts[2], verts[1], verts[3]);
+  }
   degToRad(x) {
     return x*(Math.PI/180);
   }
