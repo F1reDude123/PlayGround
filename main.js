bd32460979ffc9f2291505d25c1bff1ca9e1d042
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-08-16 04:35:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,10 +11,8 @@ export default class Scene {
     this.ctx = this.canvas.getContext("2d");
     this.fov = 500;
     this.cam = new Transform(new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-    window.onload=function(){
-      document.body.appendChild(this.canvas);
-      this.draw();
-    }
+    document.body.appendChild(this.canvas);
+    this.draw();
   }
   project(x) {
     return new Vector2(this.canvas.width/2+x.x*this.fov/(x.z+this.fov), this.canvas.height/2-x.y*this.fov/(x.z+this.fov));
