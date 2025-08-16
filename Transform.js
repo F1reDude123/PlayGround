243dbac80f95d9ccc466c8e69c5e2b3bfa4e8df3
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-08-15 22:19:16 -0400

    Update Transform.js

--- Transform.js
+++ Transform.js
@@ -3,6 +3,7 @@ export default class Transform {
     this.pos=pos;
     this.rot=rot;
     this.scale=scale;
+    this.pivot=new Vector3(this.scale.x/2, this.scale.y/2, this.scale.z/2);
   }
   setPivotPoint(x) {
     this.pivot=x;
