5a3fae5d77beff9fa784ce3a2b14daa7fe881e0f
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-08-15 21:11:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ export default class Scene {
   }
   plane(t){
     this.polygon(new Vector3(-t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos), new Vector3(t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos), new Vector3(-t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos));    
-    this.polygon(new Vector3(t.scale.x/2, 0, -t.scale.z/2)..sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos), new Vector3(-t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos), new Vector3(t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos));
+    this.polygon(new Vector3(t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos), new Vector3(-t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos), new Vector3(t.scale.x/2, 0, -t.scale.z/2).sub(t.pos).rotateX(t.rot.x).rotateY(t.rot.y).rotateZ(t.rot.z).add(t.pos));
   }
   degToRad(x) {
     return x*(Math.PI/180);
