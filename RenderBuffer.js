0e511a8e89fa72df120b185af2ae27c5c58e075c
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-10-29 15:03:59 -0400

    Update RenderBuffer.js

--- RenderBuffer.js
+++ RenderBuffer.js
@@ -4,6 +4,11 @@ import Vector2 from "https://f1redude123.github.io/PlayGround/Vector2.js";
 export default class RenderBuffer {
   vertices=[];
   indices=[];
+  properties={
+    xRot: 0,
+    yRot: 0,
+    zRot: 0
+  };
   constructor(s) {
     this.scene=s;
   }
@@ -11,13 +16,31 @@ export default class RenderBuffer {
     this.vertices=v;
     this.indices=i;
   }
+
+  rotate(v) {
+    [this.properties.xRot, this.properties.yRot, this.properties.zRot]=[v.x, v.y, v.z];
+  }
+  rotateX(x) {
+    this.properties.xRot=x;
+  }
+  rotateY(y) {
+    this.properties.yRot=y;
+  }
+  rotateZ(z) {
+    this.properties.zRot=z;
+  }
+  
   #project(x) {
     return new Vector2(this.scene.canvas.width/2+x.x*this.scene.fov/(x.z+this.scene.fov), this.scene.canvas.height/2-x.y*this.scene.fov/(x.z+this.scene.fov));
   }
   getBufferData() {
     var data=[];
     for (var i=0;i<this.indices.length;i+=3) {
-      data.push(new Polygon(this.#project(this.vertices[this.indices[i]]), this.#project(this.vertices[this.indices[i+1]]), this.#project(this.vertices[this.indices[i+2]])));
+      data.push(new Polygon(
+        this.#project(this.vertices[this.indices[i]].rotateX(this.properties.xRot).rotateY(this.properties.yRot).rotateZ(this.properties.zRot)),
+        this.#project(this.vertices[this.indices[i+1]].rotateX(this.properties.xRot).rotateY(this.properties.yRot).rotateZ(this.properties.zRot)),
+        this.#project(this.vertices[this.indices[i+2]].rotateX(this.properties.xRot).rotateY(this.properties.yRot).rotateZ(this.properties.zRot))
+      ));
     }
     return data;
   }
