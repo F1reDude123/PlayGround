d583259e6155e93aacb446325664492de9c32c73
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-10-29 10:25:49 -0400

    Update RenderBuffer.js

--- RenderBuffer.js
+++ RenderBuffer.js
@@ -7,12 +7,15 @@ export default class RenderBuffer {
     this.vertices=v;
     this.indices=i;
   }
+  #project(x) {
+    return new Vector2(this.canvas.width/2+x.x*this.fov/(x.z+this.fov), this.canvas.height/2-x.y*this.fov/(x.z+this.fov));
+  }
   getBufferData() {
     var data=[];
     var last3=[];
     for (var i=0;i<this.vertices.length;i++) {
       if (i%3==0) {
-        data.push(new Polygon(this.vertices[last3[0]], this.vertices[last3[1]], this.vertices[last3[2]]));
+        data.push(new Polygon(this.#project(this.vertices[last3[0]]), this.#project(this.vertices[last3[1]]), this.#project(this.vertices[last3[2]])));
       }
       last3.push(this.indices[i]);
     }
