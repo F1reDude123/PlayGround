1527dc78cd793787efa7aecc2a00b920dfcb81d3
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-10-29 15:15:50 -0400

    Update RenderBuffer.js

--- RenderBuffer.js
+++ RenderBuffer.js
@@ -4,19 +4,32 @@ import Vector2 from "https://f1redude123.github.io/PlayGround/Vector2.js";
 export default class RenderBuffer {
   vertices=[];
   indices=[];
-  properties={
+  #properties={
     xRot: 0,
     yRot: 0,
-    zRot: 0
+    zRot: 0,
   };
+  getBufferProperties={
+    rotate: function(v) {
+      [this.properties.xRot, this.properties.yRot, this.properties.zRot]=[v.x, v.y, v.z];
+    },
+    rotateX: function(x) {
+      this.properties.xRot=x;
+    },
+    rotateY: function(y) {
+      this.properties.yRot=y;
+    },
+    rotateZ: function(z) {
+      this.properties.zRot=z;
+    }
+  }
   constructor(s) {
     this.scene=s;
   }
   load(v, i) {
     this.vertices=v;
     this.indices=i;
-  }
-  
+  }  
   #project(x) {
     return new Vector2(this.scene.canvas.width/2+x.x*this.scene.fov/(x.z+this.scene.fov), this.scene.canvas.height/2-x.y*this.scene.fov/(x.z+this.scene.fov));
   }
@@ -24,9 +37,9 @@ export default class RenderBuffer {
     var data=[];
     for (var i=0;i<this.indices.length;i+=3) {
       data.push(new Polygon(
-        this.#project(this.vertices[this.indices[i]].rotateX(this.properties.xRot).rotateY(this.properties.yRot).rotateZ(this.properties.zRot)),
-        this.#project(this.vertices[this.indices[i+1]].rotateX(this.properties.xRot).rotateY(this.properties.yRot).rotateZ(this.properties.zRot)),
-        this.#project(this.vertices[this.indices[i+2]].rotateX(this.properties.xRot).rotateY(this.properties.yRot).rotateZ(this.properties.zRot))
+        this.#project(this.vertices[this.indices[i]].rotateX(this.#properties.xRot).rotateY(this.#properties.yRot).rotateZ(this.#properties.zRot)),
+        this.#project(this.vertices[this.indices[i+1]].rotateX(this.#properties.xRot).rotateY(this.#properties.yRot).rotateZ(this.#properties.zRot)),
+        this.#project(this.vertices[this.indices[i+2]].rotateX(this.#properties.xRot).rotateY(this.#properties.yRot).rotateZ(this.#properties.zRot))
       ));
     }
     return data;
