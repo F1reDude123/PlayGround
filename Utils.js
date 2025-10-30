8ba833669fa6771574c85b568913603886b4f547
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-10-30 09:10:01 -0400

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -1,7 +1,8 @@
-export function degToRad(x) {
-  return x*(Math.PI/180);
-}
-
-export function getFile(x) {
-  fetch(x).then(e=>e.blob()).then(blob=>{return new File([blob], "mesh.obj")});
+export default var Utils={
+  degToRad: (x) => {
+    return x*(Math.PI/180);
+  },
+  getFile: (x) => {
+    fetch(x).then(e=>e.blob()).then(blob=>{return new File([blob], "mesh.obj")});
+  }
 }
